0bbed99c20e26e5d6e7dff7ad14acb6d2557ccec
Madhumita Rano <rano.madhumita@gmail.com>
2024-06-26 12:34:17 -0700

    Adding option

--- examples/driver_numpyVector.py
+++ examples/driver_numpyVector.py
@@ -1,6 +1,6 @@
 import numpy as np
 import scipy as sp
-from util_funcs import find_nearest, lowdinOrtho
+from util_funcs import find_nearest
 from util_funcs import headerBot
 import warnings
 import time
@@ -8,19 +8,32 @@ import util
 from numpyVector import NumpyVector
 from inexact_Lanczos import inexactDiagonalization
 
-n = 2500
-ev = np.linspace(1,1400,n)
+
+# Default small matrix
+n = 100
+ev = np.linspace(1,300,n)
 np.random.seed(10)
+target = 30
+maxit = 4
+L = 6
+eConv = 1e-8
+optionDict = {"linearSolver":"gcrotmk","linearIter":1000,"linear_tol":1e-4}
+
+
+largerDenserSpetra = False
+if largerDenserSpetra:
+    n = 2500
+    ev = np.linspace(1,1400,n)
+    np.random.seed(10)
+    target = 1290
+    maxit = 20
+    L = 50
+    eConv = 1e-12
+    optionDict = {"linearSolver":"gcrotmk","linearIter":5000,"linear_tol":1e-4}
+    
 Q = sp.linalg.qr(np.random.rand(n,n))[0]
 A = Q.T @ np.diag(ev) @ Q
-
-target = 1290
-maxit = 20 
-L = 50 
-eConv = 1e-12
-    
-optionDict = {"linearSolver":"gcrotmk","linearIter":5000,"linear_tol":2e-1}
-status = {"writeOut": True,"writePlot": True}
+status = {"writeOut": False,"writePlot": False}
 Y0 = NumpyVector(np.random.random((n)),optionDict)
 sigma = target
 
