3c45c4e743b04424415fa81ca86f6271b4caf3ec
Madhumita Rano <rano.madhumita@gmail.com>
2024-06-28 15:28:44 -0700

    Maxit and MaxL

--- unittests/test_lanczosLINDEP.py
+++ unittests/test_lanczosLINDEP.py
@@ -14,19 +14,21 @@ class Test_lanczos(unittest.TestCase):
         ev = np.linspace(1,1400,n)
         np.random.seed(10)
         Q = la.qr(np.random.rand(n,n))[0]
-        A = Q.T @ np.diag(ev) @ Q
+        #A = Q.T @ np.diag(ev) @ Q
+        A = np.load("A.npy")
 
         optionDict = {"linearSolver":"gcrotmk","linearIter":5000,"linear_tol":2e-1}
         self.printChoices = {"writeOut": False,"writePlot": False}
-        Y0 = NumpyVector(np.random.random((n)),optionDict)
+        #Y0 = NumpyVector(np.random.random((n)),optionDict)
+        Y0 = NumpyVector(np.load("Y0.npy"))
         
         self.guess = Y0
         self.mat = A
         self.ev = ev 
         self.sigma = 1290
         self.eShift = 0.0
-        self.L = 50
-        self.maxit = 20
+        self.L = 100     # make sufficiently large to get LINDEP
+        self.maxit = 1000 # same as above
         self.eConv = 1e-12
 
         evEigh, uvEigh = np.linalg.eigh(A)
@@ -38,7 +40,7 @@ class Test_lanczos(unittest.TestCase):
         check if status["lindep"] is indeed True or not'''
 
         status = inexactDiagonalization(self.mat,self.guess,self.sigma,
-                self.L,1,self.eConv,self.printChoices)[2]
+                self.L,self.maxit,self.eConv,self.printChoices)[2]
         self.assertTrue(status["lindep"]== True)
 
         
@@ -55,12 +57,12 @@ class Test_lanczos(unittest.TestCase):
     def test_futileRestarts(self):
         ''' For this specific case, number of futile restarts is larger than 3'''
 
-        maxit = 40 # enough to get futileRestart > 3
         eConv = 1e-14 # stoping from early convergence
         status = inexactDiagonalization(self.mat,self.guess,self.sigma,
-                self.L,maxit,eConv,self.printChoices)[2]
+                self.L,self.maxit,eConv,self.printChoices)[2]
         nfutileRestarts = status["futileRestart"]
-        self.assertTrue(nfutileRestarts > 3)
+        if status["outerIter"] < status["maxit"]-1:
+            self.assertTrue(nfutileRestarts > 3)
 
 if __name__ == "__main__":
     unittest.main()
